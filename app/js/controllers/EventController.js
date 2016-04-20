bd8c5b0ebe05d23297a7a758a00bd2cc178f0afe
Anna Blackwell <annablackwell44@gmail.com>
2016-04-20 14:45:17 +1200

    voting widget is working

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -18,23 +18,32 @@ eventsApp.controller('EventController',
           creatorName: 'Bob Smith',
           duration: '1 hr',
           level: 'Advanced',
-          abstract: 'In this session you will learn the ins and outs of Directives'
+          abstract: 'In this session you will learn the ins and outs of Directives',
+          upVoteCount: 0
         },
         {
           name: "Scopes for fun and profit",
           creatorName: 'John Doe',
           duration: '30 mins',
           level: 'Introductory',
-          abstract: 'This session will take a closer look at scopes'
+          abstract: 'This session will take a closer look at scopes',
+          upVoteCount: 0
         },
         {
           name: "Well Behaved Controllers",
           creatorName: 'Jane Doe',
           duration: '2 hrs',
           level: 'Intermediate',
-          abstract: 'Controllers are the beginning of everything Angular does'
+          abstract: 'Controllers are the beginning of everything Angular does',
+          upVoteCount: 0
         }
       ]
+    }
+    $scope.upVoteSession = function(session) {
+      session.upVoteCount++
+    }
+    $scope.downVoteSession = function(session) {
+      session.upVoteCount--
     }
   }
 )
