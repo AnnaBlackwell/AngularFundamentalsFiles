acc4045683f35ab097bb86550f444b65a4a1242f
Anna Blackwell <annablackwell44@gmail.com>
2016-04-20 12:52:07 +1200

    controllers and markup section partially done

---
+++ app/js/controllers/EventController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+eventsApp.controller('EventController',
+  function EventController($scope) {
+    $scope.event = {
+      name: 'Angular Boot Camp',
+      date: '1/1/2013',
+      time: '10:30 am',
+      location: {
+        address: 'Google Headquarters',
+        city: 'Mountain View',
+        province: 'CA'
+      },
+      imageUrl: '/img/angularjs-logo.png',
+      sessions: [
+        {
+          name: 'Directives Masterclass',
+          creatorName: 'Bob Smith',
+          duration: '1 hr',
+          level: 'Advanced',
+          abstract: 'In this session you will learn the ins and outs of Directives'
+        },
+        {
+          name: "Scopes for fun and profit",
+          creatorName: 'John Doe',
+          duration: '30 mins',
+          level: 'Introductory',
+          abstract: 'This session will take a closer look at scopes'
+        },
+        {
+          name: "Well Behaved Controllers",
+          creatorName: 'Jane Doe',
+          duration: '2 hrs',
+          level: 'Intermediate',
+          abstract: 'Controllers are the beginning of everything Angular does'
+        }
+      ]
+    }
+  }
+)
